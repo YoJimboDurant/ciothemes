3369b4ee538fa45a8a42d1497a62f43bf5e99c36
unknown <Hzd3@L306635.cdc.gov>
2014-04-10 13:55:01 -0400

    Added cio_pull; scale_fill_nceh; and scale_fill_cio

--- R/theme_mmwr.R
+++ R/theme_mmwr.R
@@ -10,8 +10,33 @@ theme_mmwr <- function(base_size=14, base_family="sans"){
            axis.title.x = element_text(vjust = -0.2),
            legend.key.size = unit(0.5, "cm"),
            axis.title.y = element_text(angle=90)                
-  ))}
+   ))}
 
 scale_fill_mmwr_blue4dark <-function(palette, ...) {
   discrete_scale("fill", "mmwr_blue4dark", manual_pal(cio_themes_data$mmwr$blue4dark), ...)
+}
+
+scale_fill_mmwr_blue4light <-function(palette, ...) {
+  discrete_scale("fill", "mmwr_blue4light", manual_pal(cio_themes_data$mmwr$blue4light), ...)
+}
+
+
+scale_fill_nceh <-function(palette, ...) {
+  discrete_scale("fill", "nceh_main", manual_pal(unname(cio_themes_data$nceh$main)), ...)
+}
+
+
+
+cio_pull <- function(cio, division){
+  #   cio <- deparse(substitute(cio))
+  #   division <- deparse(substitute(division))
+  mycolors <- cio_themes_data[[cio]][[division]]
+  if(is.null(mycolors[[1]])) stop("Unidentified CIO or Division")
+  return(mycolors)
+}
+
+scale_fill_cio <-function(palette, cio, division,...) {
+  cio <- deparse(substitute(cio))
+  division <- deparse(substitute(division))
+  discrete_scale("fill", "cio", manual_pal(unname(cio_pull(cio,division))))
 }
--- themetest.R
+++ themetest.R
@@ -12,14 +12,14 @@ esoph$tobacco<-paste0(esoph$tobgp, "    ") #spaces for legend formatting
 
 #this is the basic plot:
 p<-ggplot(esoph, aes(x=agegp, weight=total, fill=tobacco))+
-    geom_bar(position="dodge")
+  geom_bar(position="dodge")
 
 #this is what it looks like:
 p
 
 #add mmwr theme and color schemes
 p+
-theme_mmwr() + scale_fill_mmwr_blue4dark(name="Daily Tobacco Consumption")
+  theme_mmwr() + scale_fill_mmwr_blue4dark(name="Daily Tobacco Consumption")
 
 # However, many stylistic properties are not part of the theme, so need to add manually:
 # ad black outline to bars (but not to legend) which requires the second geom_bar()
@@ -27,10 +27,18 @@ theme_mmwr() + scale_fill_mmwr_blue4dark(name="Daily Tobacco Consumption")
 # no simple way in ggplot to create vertical space between fill boxes in legend because 
 #     fill colors expand to the entire legend.key.size (but shapes do not)
 #     as a compromise for now, I added spaces to the category labels and used a horizontal legend.
- 
+
 p+
   theme_mmwr()+ scale_fill_mmwr_blue4dark(name="Daily Tobacco Consumption")+
   geom_bar(position="dodge", colour="black", show_guide=FALSE)+
   scale_y_continuous(expand = c(0,0), limits=c(0,150))+
   theme(legend.position="bottom")
-  
+
+p+theme_mmwr()+scale_fill_nceh()
+
+# test cio pull function so we don't have to do individual fills
+
+p+theme_mmwr()+ scale_fill_cio(cio=ncbddd, division=main)
+p+theme_mmwr()+ scale_fill_cio(cio=mmwr, division=blue4dark)
+p+theme_mmwr()+ scale_fill_cio(cio=mmwr, division=blue4light)
+p+theme_mmwr()+ scale_fill_cio(cio=mmwr, division=browns3)
